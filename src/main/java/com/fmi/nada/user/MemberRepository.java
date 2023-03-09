6abf4db846d16ef9afabada98f1a209b882a705c
Han-Bin Song <thdgksq@naver.com>
2023-03-09 17:20:12 +0900

    Implement DTO, Repository in user
    회원가입에 쓰일 DTO, Repository 구현완료

---
+++ src/main/java/com/fmi/nada/user/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.fmi.nada.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * 회원 레포지토리
+ */
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    public Member findByUsername(String name);
+
+}
