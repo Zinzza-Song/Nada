985521ed3ccb27057d8716de5627d793c8a147e7
Han-Bin Song <thdgksq@naver.com>
2023-03-09 17:16:38 +0900

    Implement Entity
    Advice, Ban, BlockList, Friends, Like, Member, Sympathy 엔티티 구현

--- src/main/java/com/fmi/nada/main/Advice.java
+++ src/main/java/com/fmi/nada/main/Advice.java
@@ -1,4 +1,28 @@
 package com.fmi.nada.main;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * 오늘의 조언 엔티티
+ */
+@Entity
+@Table
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Advice {
+
+    @GeneratedValue
+    @Id
+    private Long advice_idx;
+
+    private String advice_content;
+    private String advice_author;
+
 }
--- src/main/java/com/fmi/nada/user/BlockList.java
+++ src/main/java/com/fmi/nada/user/BlockList.java
@@ -1,4 +1,33 @@
 package com.fmi.nada.user;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+/**
+ * 차단 유저 목록 엔티티
+ */
+@Entity
+@Table
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class BlockList {
+
+    @GeneratedValue
+    @Id
+    private Long block_member_idx;
+
+    private Long member_idx;
+    private String block_member_nickname;
+    private String block_member_reason;
+
+    @CreatedDate
+    private LocalDateTime block_member_date;
+
 }
