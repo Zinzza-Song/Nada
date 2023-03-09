bbfb2a7bb4aefe35cc8437667b74cbf91bef8e28
Han-Bin Song <thdgksq@naver.com>
2023-03-09 10:37:49 +0900

    Initial commit

---
+++ src/main/java/com/fmi/nada/NadaApplication.java
@@ -0,0 +1,13 @@
+package com.fmi.nada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NadaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NadaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fmi/nada/NadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmi.nada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NadaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
