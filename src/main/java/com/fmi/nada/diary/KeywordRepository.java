07a0b41016af4dd0d480b584509140882fe71ee3
Han-Bin Song <thdgksq@naver.com>
2023-03-13 14:55:58 +0900

    Apply Top5KeywordList to main page
    Top5KeywordList 메인페이지에 불러오는 기능 적용

--- src/main/java/com/fmi/nada/diary/KeywordRepository.java
+++ src/main/java/com/fmi/nada/diary/KeywordRepository.java
@@ -1,11 +1,16 @@
 package com.fmi.nada.diary;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
 /**
  * 키워드 레포지토리
  * */
 public interface KeywordRepository extends JpaRepository<Keyword, Long> {
 
     public Keyword findByKeywordName(String keywordName);
+
+    public List<Keyword> findTop5ByOrderByKeywordCntDesc();
 
 }
--- src/main/java/com/fmi/nada/main/MainController.java
+++ src/main/java/com/fmi/nada/main/MainController.java
@@ -1,6 +1,7 @@
 package com.fmi.nada.main;
 
 import com.fmi.nada.diary.Keyword;
+import com.fmi.nada.diary.KeywordService;
 import com.fmi.nada.user.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
@@ -16,6 +17,7 @@ import java.util.List;
 public class MainController {
 
     private final MainService mainService;
+    private final KeywordService keywordService;
 
     /**
      * 오늘의 조언 랜덤 조회
@@ -23,14 +25,13 @@ public class MainController {
      * @return 메인 페이지 view("/index.html")
      */
     @GetMapping("/")
-    public String main(Model model, Authentication authentication) {
-        if(authentication != null){
-            Member member = (Member) authentication.getPrincipal();
-            model.addAttribute("loginMember", member);
-        }
+    public String main(Model model) {
 
         Advice advice = mainService.findByAll().get(0);
         model.addAttribute("adviceModel", advice);
+
+        List<Keyword> keywordList = keywordService.findTop5ByOrderByKeywordCntDesc();
+        model.addAttribute("keywordList", keywordList);
 
         return "index";
     }
