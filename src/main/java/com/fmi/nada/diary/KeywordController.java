346e76cc609f974bf961dcafd91507c980095a0a
keisa <skycindy89@naver.com>
2023-03-13 09:39:45 +0900

    미사용 임포트 삭제

--- src/main/java/com/fmi/nada/diary/KeywordController.java
+++ src/main/java/com/fmi/nada/diary/KeywordController.java
@@ -2,8 +2,6 @@ package com.fmi.nada.diary;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
