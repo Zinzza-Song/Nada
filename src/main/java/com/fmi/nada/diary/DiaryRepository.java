ef729794db3ef357cc82cdd87ff2b91200cb9947
kunyoung <rlarjsdyd3@gmail.com>
2023-03-14 18:25:57 +0900

    diary paging 수정중

--- src/main/java/com/fmi/nada/diary/DiaryRepository.java
+++ src/main/java/com/fmi/nada/diary/DiaryRepository.java
@@ -1,7 +1,10 @@
 package com.fmi.nada.diary;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
@@ -18,6 +21,6 @@ public interface DiaryRepository extends JpaRepository<Diary, Long> {
     Diary findByDiarySubject(String diarySubject);
 
     // 최신 다이어리 게시글부터 정렬
-    List<Diary> findAllByOrderByDiaryDateDesc();
+    Page<Diary> findAllByOrderByDiaryDateDesc(Pageable pageable);
 
 }
