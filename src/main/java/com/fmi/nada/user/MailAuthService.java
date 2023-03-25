99d98717a027b76a079b0efa173c3c33cfa2b2a6
Han-Bin Song <thdgksq@naver.com>
2023-03-25 14:31:28 +0900

    메일 인증 코드 버그 픽스
    회원가입과 비밀번호 찾기를 한 유저가 실행할 시 코드가 바뀌지않고 똑같은 코드가 생성되는 버그 픽스

--- src/main/java/com/fmi/nada/user/MailAuthService.java
+++ src/main/java/com/fmi/nada/user/MailAuthService.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.RequestScope;
 
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -16,6 +17,7 @@ import java.util.Random;
 @Slf4j
 @RequiredArgsConstructor
 @Service
+@RequestScope
 public class MailAuthService {
 
 
