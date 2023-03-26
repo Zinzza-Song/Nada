d68088d7b0bfae245b8b69bd239d1b6e908685da
geonsang <skycindy89@naver.com>
2023-03-27 05:31:15 +0900

    회원가입메일, 비밀번호 찾기메일 메일본문내용 투트랙으로 변경적용

--- src/main/java/com/fmi/nada/user/MailAuthService.java
+++ src/main/java/com/fmi/nada/user/MailAuthService.java
@@ -25,7 +25,9 @@ public class MailAuthService {
 
     private final String ePw = createKey();
 
-    public MimeMessage createMessage(String to) throws MessagingException, UnsupportedEncodingException {
+
+    //회원가입 인증 메일작성
+    public MimeMessage createMessageJoin(String to) throws MessagingException, UnsupportedEncodingException {
         System.out.println("메일받을 사용자" + to);
         System.out.println("인증번호" + ePw);
 
@@ -58,6 +60,41 @@ public class MailAuthService {
 
     }
 
+    //비밀번호 찾기 인증메일 작성
+    public MimeMessage createMessageFindPassword(String to) throws MessagingException, UnsupportedEncodingException {
+        System.out.println("메일받을 사용자" + to);
+        System.out.println("인증번호" + ePw);
+
+        MimeMessage message = javaMailSender.createMimeMessage();
+
+        message.addRecipients(Message.RecipientType.TO, to);
+        message.setSubject("[NADA] 비밀번호 찾기를 위한 이메일 인증코드 입니다.");
+
+        String msgg = "";
+        msgg += "<h1>안녕하세요</h1>";
+        msgg += "<h1>나만의 감성다이어리 NADA 입니다.</h1>";
+        msgg += "<br>";
+        msgg += "<p>아래 인증코드를 비밀번호 찾기 페이지에 입력해주세요</p>";
+        msgg += "<br>";
+        msgg += "<br>";
+        msgg += "<div align='center' style='border:1px solid black'>";
+        msgg += "<h3 style='color:blue'>비밀번호 찾기 인증코드 입니다</h3>";
+        msgg += "<div style='font-size:130%'>";
+        msgg += "<strong>" + ePw + "</strong></div><br/>"; // 메일에 인증번호 ePw 넣기
+        msgg += "</div>";
+
+        message.setText(msgg, "utf-8", "html");
+        //발송할 메시지, 캐릭터셋, 타입
+        message.setFrom(new InternetAddress("skycindy89@naver.com", "NADA_Admin"));
+        // 발송하는 사람의 메일주소, 이름
+        System.out.println("createmessage 에서 생성된 msgg 메시지" + msgg);
+        System.out.println("createmessage 에서 생성된 리턴 메시지" + message);
+
+        return message;
+
+    }
+
+    //인증번호 생성
     public String createKey() {
         StringBuffer key = new StringBuffer();
         Random rnd = new Random();
@@ -69,10 +106,26 @@ public class MailAuthService {
         return key.toString();
     }
 
-    public String sendSimpleMessage(String to) throws Exception {
+    //회원가입 메일 발송
+    public String sendSimpleMessageJoin(String to) throws Exception {
         //변수로 들어온 to는 메일주소가된다(받을주소)
         //MimeMessage 객체 안에 내가 전송할 메일 주소를 넣음
-        MimeMessage message = createMessage(to);
+        MimeMessage message = createMessageJoin(to);
+        // 메일발송
+        try { // 예외처리
+            javaMailSender.send(message);
+        } catch (MailException e) {
+            e.printStackTrace();
+            throw new IllegalArgumentException();
+        }
+        return ePw; // 메일로 보냈던 인증코드를 서버로 반환(비교하기위해서)
+    }
+
+    //비밀번호 찾기 메일 발송
+    public String sendSimpleMessageFindPassword(String to) throws Exception {
+        //변수로 들어온 to는 메일주소가된다(받을주소)
+        //MimeMessage 객체 안에 내가 전송할 메일 주소를 넣음
+        MimeMessage message = createMessageFindPassword(to);
         // 메일발송
         try { // 예외처리
             javaMailSender.send(message);
