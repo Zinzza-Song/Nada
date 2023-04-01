5854c3fa150b19fa8116d9d42d51334ef5061b86
Han-Bin Song <thdgksq@naver.com>
2023-04-02 00:18:24 +0900

    social 적용 rnd 진행 중

--- src/main/java/com/fmi/nada/config/SpringSecurityConfig.java
+++ src/main/java/com/fmi/nada/config/SpringSecurityConfig.java
@@ -1,22 +1,20 @@
 package com.fmi.nada.config;
 
-import com.fmi.nada.admin.LogRepository;
-import com.fmi.nada.user.Member;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fmi.nada.jwt.JwtService;
+import com.fmi.nada.login.LoginService;
+import com.fmi.nada.oauth2.CustomOAuth2UserService;
+import com.fmi.nada.oauth2.OAuth2LoginFailureHandler;
+import com.fmi.nada.oauth2.OAuth2LoginSuccessHandler;
 import com.fmi.nada.user.MemberRepository;
-import com.fmi.nada.user.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.web.access.AccessDeniedHandler;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.SecurityFilterChain;
 
 /**
  * Security 설정
@@ -26,124 +24,31 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @RequiredArgsConstructor
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final MemberService memberService;
+    private final LoginService loginService;
+    private final JwtService jwtService;
+    private final ObjectMapper objectMapper;
+    private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
+    private final OAuth2LoginFailureHandler oAuth2LoginFailureHandler;
+    private final CustomOAuth2UserService customOAuth2UserService;
     private final MemberRepository memberRepository;
-    private final LogRepository logRepository;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic().disable();
-        http.csrf().disable();
-        http.rememberMe().disable();
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
-//        http.addFilterBefore(
-//                new JwtAuthenticationFilter(authenticationManager(), logRepository),
-//                UsernamePasswordAuthenticationFilter.class
-//        ).addFilterBefore(
-//                new JwtAuthorizationFilter(memberRepository),
-//                BasicAuthenticationFilter.class
-//        );
-
-        http.authorizeRequests()
-//                .antMatchers("/", "/home", "/user/join/**", "/user/reset_password/**",
-//                        "/diary", "/diary/read", "/board/notice", "/board/notice/read", "/board/QNA",
-//                        "/user/login", "/denied").permitAll()
-//                .antMatchers("/user/modify/**", "/user/delete", "/user/read",
-//                        "/user/friend_list", "/user/friend_add", "/user/friend_del", "/user/block_list",
-//                        "/user/unblock", "/reporting/**", "/diary/**", "/board/QNA/write/**",
-//                        "/board/QNA/read", "/board/QNA/delete", "board/report/**").hasRole("USER")
-//                .antMatchers("/**").hasRole("ADMIN")
-//                .anyRequest().authenticated();
-                .anyRequest().permitAll();
-
-        http.formLogin()
-                .loginPage("/login")
-                .defaultSuccessUrl("/")
-                .permitAll();
-
-        http.logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .invalidateHttpSession(true)
-//                .addLogoutHandler(((request, response, authentication) -> {
-//                    String token = "";
-//                    try {
-//                        token = Arrays.stream(request.getCookies())
-//                                .filter(cookie -> cookie.getName().equals(JwtProperties.COOKIE_NAME))
-//                                .findFirst()
-//                                .map(Cookie::getValue)
-//                                .orElse(null);
-//                    } catch (Exception ignored) {
-//                    }
-//                    String userName = JwtUtils.getUsername(token);
-//                    Member member = memberRepository.findByUsername(userName);
-//                    logRepository.save(new Log(
-//                            member.getMemberIdx(),
-//                            true,
-//                            member.getUsername(),
-//                            "로그아웃"
-//                    ));
-//                })).addLogoutHandler(((request, response, authentication) -> {
-//                    String token = "";
-//                    try {
-//                        token = Arrays.stream(request.getCookies())
-//                                .filter(cookie -> cookie.getName().equals(JwtProperties.COOKIE_NAME))
-//                                .findFirst()
-//                                .map(Cookie::getValue)
-//                                .orElse(null);
-//                    } catch (Exception ignored) {
-//                    }
-//                    String userName = JwtUtils.getUsername(token);
-//                    Member member = memberRepository.findByUsername(userName);
-//                    logRepository.save(new Log(
-//                            member.getMemberIdx(),
-//                            true,
-//                            member.getUsername(),
-//                            "로그아웃"
-//                    ));
-//                }))
-                .logoutSuccessHandler(((request, response, authentication) -> {
-                    response.sendRedirect("/user/login");
-                }));
-//                .deleteCookies(JwtProperties.COOKIE_NAME);
-
-        http.exceptionHandling()
-                .accessDeniedHandler(accessDeniedHandler());
-    }
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        // 정적 리소스 spring security 대상에서 제외 web.ignoring().antMatchers("/images/**", "/css/**"); 아래 코드와 같은 내용
-        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
-    }
-
-    /**
-     * UserDetailsService 구현
-     *
-     * @return UserDetailsService
-     */
     @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-        return username -> {
-            Member member = memberService.findByUsername(username);
-            if (member == null)
-                throw new UsernameNotFoundException(username);
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .formLogin().disable() // FormLogin 사용 X
+                .httpBasic().disable() // HttpBasic 사용 X
+                .csrf().disable() // csrf 보안 사용 X
+                .headers().frameOptions().disable()
+                .and()
 
-            return member;
-        };
-    }
+                // 세션 사용 X
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
 
-    /**
-     * 접근 거부 예외처리 구현
-     *
-     * @return accessDeniedHandler
-     */
-    @Bean
-    public AccessDeniedHandler accessDeniedHandler() {
-        CustomAccessDeniedHandler accessDeniedHandler = new CustomAccessDeniedHandler("/denied");
+                // URL별 권한 관리 옵션
+                .authorizeRequests();
 
-        return accessDeniedHandler;
+        return http.build();
     }
 
 }
--- src/main/java/com/fmi/nada/oauth2/OAuth2LoginSuccessHandler.java
+++ src/main/java/com/fmi/nada/oauth2/OAuth2LoginSuccessHandler.java
@@ -1,5 +1,8 @@
 package com.fmi.nada.oauth2;
 
+import com.fmi.nada.jwt.JwtService;
+import com.fmi.nada.user.Member;
+import com.fmi.nada.user.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -13,8 +16,44 @@ import java.io.IOException;
 @Component
 @RequiredArgsConstructor
 public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
-    @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
 
+    private final JwtService jwtService;
+    private final MemberRepository memberRepository;
+
+    @Override
+    public void onAuthenticationSuccess(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Authentication authentication
+    ) throws IOException, ServletException {
+        try {
+            CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
+
+            // Member의 Authority가 GUEST일 경우 처음 요청한 회원이므로 회원가입 페이지로 리다이렉트
+            if (oAuth2User.getAuthority().equals("ROLE_GUEST")) {
+                String accessToken = jwtService.createAccessToken(oAuth2User.getEmail());
+                response.addHeader(jwtService.getAccessHeader(), "Bearer " + accessToken);
+                response.sendRedirect("user/join"); // 회원가입 주가 정보 입력 폼으로 리다이랙트
+
+                jwtService.sendAccessAndRefreshToken(response, accessToken, null);
+                Member findMember = memberRepository.findByUsername(oAuth2User.getEmail())
+                        .orElseThrow(() -> new IllegalArgumentException("이메일에 해당하는 유저가 없습니다."));
+                findMember.authorizeUser();
+            } else
+                loginSuccess(response, oAuth2User);
+        } catch (Exception e) {
+            throw e;
+        }
     }
+
+    private void loginSuccess(HttpServletResponse response, CustomOAuth2User oAuth2User) throws IOException {
+        String accessToken = jwtService.createAccessToken(oAuth2User.getEmail());
+        String refreshToken = jwtService.createRefreshToken();
+        response.addHeader(jwtService.getAccessHeader(), "Bearer " + accessToken);
+        response.addHeader(jwtService.getRefreshHeader(), "Bearer " + refreshToken);
+
+        jwtService.sendAccessAndRefreshToken(response, accessToken, refreshToken);
+        jwtService.updateRefreshToken(oAuth2User.getEmail(), refreshToken);
+    }
+
 }
