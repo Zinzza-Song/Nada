af2f0054cacac8819f4a303284e4677a96367005
Han-Bin Song <thdgksq@naver.com>
2023-04-02 14:05:16 +0900

    메일인증 버그 픽스

--- src/main/java/com/fmi/nada/user/mailConfig.java
+++ src/main/java/com/fmi/nada/user/mailConfig.java
@@ -1,5 +1,6 @@
 package com.fmi.nada.user;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -10,6 +11,12 @@ import java.util.Properties;
 @Configuration
 public class mailConfig {
 
+    @Value("${mail.mail-id}")
+    private String mailId;
+
+    @Value("${mail.mail-pw}")
+    private String mailPw;
+
     @Bean
     public JavaMailSender javaMailSender() {
 
@@ -17,9 +24,9 @@ public class mailConfig {
 
         javaMailSender.setHost("smtp.naver.com");
         // 호스트(네이버 사용) 네이버 메일 - > 환경설정 -> POP3/IMAP 설정 -> POP3/SMTP 설정 -> 하단의 SMTP 서버명
-        javaMailSender.setUsername("skycindy89@naver.com");
+        javaMailSender.setUsername(mailId);
         // 서버 열어놓은 계정 입력
-        javaMailSender.setPassword("XTK2C46XJU8Y");
+        javaMailSender.setPassword(mailPw);
         // 서버 열어놓은 계정의 어플리케이션 비밀번호 입력
         javaMailSender.setPort(465);
         // 네이버 SMTP 포트 465로 고정되어 있음
